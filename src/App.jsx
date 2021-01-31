50816ce62a89fe7ba55092aa2c2ec474f55a8095
Ikeuchi Ryuto <ryu.i7133@gmail.com>
2021-01-31 18:35:47 +0900

    curl api firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // import { render } from '@testing-library/react';
 import React from 'react';
-import defaultDataset from './dataset.json';
 import './assets/styles/style.css';
 import { AnswersList, Chats } from './components/index';
 import FormDialog from './components/Forms/FormDialog';
+import {db} from './firebase/index'
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -11,7 +12,7 @@ export default class App extends React.Component {
       answers: [],
       chats:[],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open:false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
@@ -60,9 +61,25 @@ export default class App extends React.Component {
     }
   };
 
+  initDataset(dataset) {
+    this.setState({dataset:dataset})
+  }
+
   componentDidMount() {
-    const initAnswer = "";
-    this.selectAnswer(initAnswer, this.state.currentId);
+    (async () => {
+      const dataset = this.state.dataset
+      
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id
+          const data = doc.data()
+          dataset[id]=data
+        })
+      })
+      this.initDataset(dataset)
+      const initAnswer = "";
+      this.selectAnswer(initAnswer, this.state.currentId);
+    })()
   };
 
   componentDidUpdate(prevProps,prevState,snapshot) {
